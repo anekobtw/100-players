81f4f30f7ecc81d4f7362faefab3086772e886f1
anekobtw <krolexonder@gmail.com>
2025-10-03 14:46:01 -0400

    added local server

---
+++ central_api.py
@@ -0,0 +1,83 @@
+import asyncio
+import aiohttp
+
+import uvicorn
+from fastapi import FastAPI, Form, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+import database
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+async def fetch_profile(nickname: str) -> dict | None:
+    url = f"https://gdbrowser.com/api/profile/{nickname}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status != 200:
+                return None
+            return await resp.json()
+
+
+blacklisted = {"snownitt"}
+winners = {
+    "anekobtw",
+    "zolotro",
+    "mrmatras",
+    "spookyz228",
+    "maskagd",
+    "forty0ne",
+}
+
+
+@app.post("/login")
+async def _(username: str = Form(...)):
+    if len(database.get_all()) >= LIMIT:
+        raise HTTPException(
+            404, detail="Мы уже набрали достаточное количество игроков для съёмок.")
+    if username in blacklisted:
+        raise HTTPException(
+            404, detail="Данный аккаунт заблокирован навсегда.")
+    if database.user_exists(username):
+        raise HTTPException(404, detail="Данный аккаунт уже зарегестрирован.")
+
+    data = await fetch_profile(username)
+
+    if not data:
+        raise HTTPException(404, detail="Такого аккаунта не существует.")
+
+    database.whitelist(
+        gd_account_id=data["accountID"],
+        gd_username=data["username"],
+        winner_role=username in winners,
+    )
+
+    return True
+
+
+@app.get("/events")
+async def _():
+    return database.list_servers()
+
+
+@app.get("/database")
+async def _():
+    users = database.get_all()
+    if not users:
+        raise HTTPException(404, "No users found")
+    return users
+
+
+if __name__ == "__main__":
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000, log_level="debug")
+    server = uvicorn.Server(config)
+    asyncio.run(server.serve())
--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# PORT 4200 - Central API
 echo "Starting FastAPI server..."
-uvicorn api:app --reload &
+uvicorn central_api:app --reload --port 4200 &
 
+# PORT 4201 - Central server
+cd server
+chmod +x ./globed-central-server-x64
+./globed-central-server-x64 &
+cd ..
+
+# PORT 4202 - Website
 echo "Starting npm website..."
 cd website
-npm run dev &
+npm run dev -- --port 4202 &
+cd ..
 
 wait
